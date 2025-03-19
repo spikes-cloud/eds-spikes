e7cb8d9ea1e9672b49b5c70507c3e70e008b83c9
Singh <gurprit.a.singh@accenture.com>
2025-03-20 02:48:32 +0530

    updated decorate function for hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,40 @@
 export default function decorate(block) {
-  console.log(block);
+  const container = document.createElement('div');
+  let heroImage = null;
+  let heroTitle = null;
+  let heroCaption = null;
+  let introText = null;
+  container.className = 'container';
+  const children = [...block.children];
+
+  children.forEach((child, index) => {
+    const innerDiv = child.querySelector('div');
+    if (!innerDiv) return;
+    const content = innerDiv.innerHTML.trim();
+
+    if (innerDiv.querySelector('picture')) {
+      heroImage = document.createElement('div');
+      heroImage.className = 'hero-image';
+      heroImage.innerHTML = innerDiv.innerHTML;
+    } else if (index === 1) {
+      heroCaption = document.createElement('div');
+      heroCaption.className = 'hero-caption';
+      heroCaption.innerHTML = content;
+    } else if (index === 2) {
+      heroTitle = document.createElement('div');
+      heroTitle.className = 'hero-title';
+      heroTitle.innerHTML = content;
+    } else if (index === 3) {
+      introText = document.createElement('div');
+      introText.className = 'hero-intro-text';
+      introText.innerHTML = content;
+    }
+  });
+
+  block.textContent = '';
+  container.append(heroTitle);
+  container.append(heroImage);
+  container.append(heroCaption);
+  container.append(introText);
+  block.append(container);
 }
