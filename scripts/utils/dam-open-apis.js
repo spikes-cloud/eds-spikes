9a73aeb1ca05e1e87612ecb476f4aa7ba6af73f7
Hemant Garg <hegarg83@gmail.com>
2025-03-23 22:15:03 +1100

    asset selector - 2025-03-23 22:15:03

--- scripts/utils/dam-open-apis.js
+++ scripts/utils/dam-open-apis.js
@@ -1,17 +1,22 @@
 import { stringFormat } from './common-utils.js';
 
-const getAssetMetadata = async (damUrlStr) => {
+/**
+ * @param {string} damUrlStr - The base DAM Open API URL.
+ * @param {string} params - Query parameters to append to the request.
+ * @returns {Promise<Object>} - The JSON object or an empty object if an error occurs.
+ */
+const getAssetMetadata = async (damUrlStr, params) => {
   try {
-    const metadataResp = await fetch(`${damUrlStr}/metadata`);
-    if (!metadataResp.ok) {
-      throw new Error(`HTTP error! Status: ${metadataResp.status}`);
-    }
-    const data = await metadataResp.json(); // Parse JSON response
+    const headers = { cache: 'no-cache' };
+    const metadataResp = await fetch(`${damUrlStr}/metadata?${params}`, headers);
+    if (!metadataResp.ok) throw new Error(`HTTP error! Status: ${metadataResp.status}`);
+
+    const data = await metadataResp.json();
     return data;
   } catch (error) {
     console.error(`DAM Metadata API failed: ${error.message}`);
   }
-  return null;
+  return '{}';
 };
 
 const OPEN_API_FORMAT = '/as/{seoname}?smartcrop={crop}&preferwebp=true';
@@ -22,30 +27,30 @@ const createPicTagWithOpenApi = async (damUrl) => {
     console.error('Invalid URL provided for DAM asset.');
     return null;
   }
+  // Step 0 - Create Picture Tag
+  const pictureEle = document.createElement('picture');
+  const params = new URLSearchParams(window.location.search);
+
   // Step 1 - Fetch Metadata
-  const metadataMap = await getAssetMetadata(damUrl?.href);
-  if (!metadataMap) {
-    console.error('Metadata retrieval failed, skipping image creation.');
-    return null;
-  }
+  const metadataMap = await getAssetMetadata(damUrl?.href, params.toString());
 
   // Step 2 - Extract metadata details
   const { repositoryMetadata, assetMetadata } = metadataMap;
-  const dcFormat = repositoryMetadata['dc:format'] || 'image/webp';
-  const title = assetMetadata['dc:title'] || '';
+  const dcFormat = repositoryMetadata?.['dc:format'] || 'image/webp';
+  const title = assetMetadata?.['dc:title'] || '';
   const seoname = `${title?.replace(/[^A-Z0-9]+/gi, '-')}-${dcFormat.replace('/', '.')}`;
 
-  // Step 3 - Create picture Tag.
-  const pictureEle = document.createElement('picture');
-
-  // Step 4 -  Add source tag & srcset attr
+  // Step 3 -  Add source tag & srcset attr
   if (repositoryMetadata?.smartcrops) {
     Object.entries(repositoryMetadata.smartcrops).forEach(([crop, value]) => {
       const smartCropUrl = stringFormat(OPEN_API_FORMAT, { seoname, crop });
       const sourceEle = document.createElement('source');
-      sourceEle.setAttribute('srcset', `${damUrl?.href}${smartCropUrl}`);
+      sourceEle.setAttribute(
+        'srcset',
+        `${damUrl?.href}${smartCropUrl}&${params.toString()}`,
+      );
       sourceEle.setAttribute('type', 'image/webp');
-
+      sourceEle.setAttribute('data-sm-key', crop);
       const mediaArgs = { widthType: 'min', width: value.width }; // TODO: Understand when to use min/max values
       const media = stringFormat(MEDIA_FORMAT, mediaArgs);
       sourceEle.setAttribute('media', media);
@@ -54,13 +59,13 @@ const createPicTagWithOpenApi = async (damUrl) => {
     });
   }
 
-  // Step 5 - Add <img> Tag
+  // Step 4 - Add <img> Tag
   const imgAtts = {
     loading: damUrl?.loading || 'lazy', // For better performance
     src: damUrl.href,
     alt: damUrl?.alt || title || 'Default Image', // Add Image Title incase the alt text is missing.
-    width: assetMetadata['tiff:ImageWidth'] || 'auto',
-    height: assetMetadata['tiff:ImageLength'] || 'auto',
+    width: assetMetadata?.['tiff:ImageWidth'] || 'auto',
+    height: assetMetadata?.['tiff:ImageLength'] || 'auto',
   };
   const imgEle = document.createElement('img');
   Object.entries(imgAtts).forEach(([attr, value]) => {
