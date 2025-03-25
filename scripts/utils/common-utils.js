7df276968db12d2112b14f98921ce31d02264170
hegarg <36055526+hegarg@users.noreply.github.com>
2025-03-25 11:54:22 +1100

    Dam pic (#7)
    * asset selector - 2025-03-24 18:24:19
    
    * asset selector - 2025-03-25 10:21:51
    
    * asset selector - 2025-03-25 11:32:55

--- scripts/utils/common-utils.js
+++ scripts/utils/common-utils.js
@@ -1,6 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-// Once we have more than one function, we can remove this elint.
-
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-globals */
 /**
  * @param {string} template - Placeholders in the format `{key}`.
  * @param {Object} values - A key-value pairs to replace in the template.
@@ -10,4 +9,33 @@ function stringFormat(template, values) {
   return template.replace(/\{(.*?)}/g, (_, key) => values[key] || '');
 }
 
-export { stringFormat };
+const sortObjectByAttr = (parentObj, attr, order = 'asc', sensitivity = 'base') => {
+  // Validate that parentObj is a non-null object
+  if (parentObj === null || typeof parentObj !== 'object') {
+    console.error('Provided input is not a valid object.');
+    return {};
+  }
+
+  // Normalise order so that only 'asc' or 'desc' are used
+  order = order.toLowerCase() === 'desc' ? 'desc' : 'asc';
+
+  return Object.fromEntries(
+    Object.entries(parentObj).sort(([, a], [, b]) => {
+      const aVal = a?.[attr] ?? '';
+      const bVal = b?.[attr] ?? '';
+      // Numbers Comparison
+      const aNum = Number(aVal);
+      const bNum = Number(bVal);
+      const bothAreNumbers = !isNaN(aNum) && !isNaN(bNum);
+      if (bothAreNumbers) {
+        return order === 'asc' ? aNum - bNum : bNum - aNum;
+      }
+
+      // String Comparison
+      const cmp = String(aVal).localeCompare(String(bVal), undefined, { sensitivity });
+      return order === 'asc' ? cmp : -cmp;
+    }),
+  );
+};
+
+export { stringFormat, sortObjectByAttr };
