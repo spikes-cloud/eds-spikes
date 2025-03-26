808556c5e45f4f425ccda0bc886565edb8374e5c
Gurprit-Developer <gurprit.a.singh@accenture.com>
2025-03-26 20:55:35 +0530

    latest hero js changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,45 +1,40 @@
-import optimizeImages from '../../scripts/utils/optimize-images.js'; // Adjust the path as needed
+// import optimizeImages from '../../scripts/utils/optimize-images.js'; // Adjust the path as needed
 import promoteChild from '../../scripts/utils/dom.js';
+import { createPicTagWithOpenApi } from '../../scripts/utils/dam-open-apis.js';
 
-export default function decorate(block) {
-  const [img, imgAlt, caption, hideOnMobile, title, heading, text, logo] = [...block.children];
+export default async function decorate(block) {
+  const [img, imgAlt, caption, hideOnMobile, title, heading, text, logo, logoAlt] = [
+    ...block.children,
+  ];
+
+  // generating damURL from anchor tag
+  const anchor = img.querySelector('a');
+  const link = anchor?.getAttribute('href');
+  const damUrl = link ? new URL(link) : block.textContent.trim();
+  const pictureEle = await createPicTagWithOpenApi(damUrl);
 
   const heroBody = document.createElement('div');
 
-  const image = img ? promoteChild(img) : null;
   const captionText = caption ? promoteChild(caption) : null;
   const titleText = title ? promoteChild(title) : null;
   const introText = text ? promoteChild(text) : null;
   const isAltTextRequired = imgAlt ? promoteChild(imgAlt) : null;
   const hideImageOnMobile = hideOnMobile ? promoteChild(hideOnMobile) : null;
-  const headingText = heading ? promoteChild(heading) : null;
-  const logoImage = logo ? promoteChild(logo) : null;
 
   // Add CSS classes only if the elements exist
-  image?.classList.add('image');
   captionText?.classList.add('caption');
   titleText?.classList.add('title');
   introText?.classList.add('intro-text');
-  headingText?.classList.add('heading');
-  logoImage?.classList.add('logo-image');
-
   heroBody.classList.add('body');
 
   // Append only the elements that exist
-  const elementsToAppend = [
-    titleText,
-    headingText,
-    image,
-    isAltTextRequired,
-    captionText,
-    introText,
-    hideImageOnMobile,
-    logoImage,
-  ].filter((el) => el !== null); // Remove `null` values
+  const elementsToAppend = [titleText, pictureEle, captionText, introText].filter(
+    (el) => el !== null,
+  ); // Remove `null` values
 
   heroBody.append(...elementsToAppend); // Append only existing elements
-
   block.textContent = '';
   block.append(heroBody);
-  optimizeImages(block);
+  // optimizeImages(block);
+  console.log(heading, logo, logoAlt, isAltTextRequired, hideImageOnMobile);
 }
