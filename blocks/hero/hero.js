391c0303f26d71348bd49b84703b8b53c2c9d7a3
huzefaakhtar53 <huzefa.akhtar@accenture.com>
2025-04-01 17:03:22 +0530

    restructured the decorate JS

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,40 +1,61 @@
-// import optimizeImages from '../../scripts/utils/optimize-images.js'; // Adjust the path as needed
+import optimizeImages from '../../scripts/utils/optimize-images.js'; // Adjust the path as needed
 import promoteChild from '../../scripts/utils/dom.js';
-import { createPicTagWithOpenApi } from '../../scripts/utils/dam-open-apis.js';
+// import { createPicTagWithOpenApi } from '../../scripts/utils/dam-open-apis.js';
 
 export default async function decorate(block) {
-  const [img, imgAlt, caption, hideOnMobile, title, heading, text, logo, logoAlt] = [
-    ...block.children,
-  ];
+  const [
+    heroImage,
+    checkImgAlt,
+    imgCaption,
+    heroTitle,
+    introText,
+    logoImg,
+    checklogoAlt,
+  ] = [...block.children];
 
   // generating damURL from anchor tag
-  const anchor = img.querySelector('a');
-  const link = anchor?.getAttribute('href');
-  const damUrl = link ? new URL(link) : block.textContent.trim();
-  const pictureEle = await createPicTagWithOpenApi(damUrl);
+  // const anchor = heroImage.querySelector('a');
+  // const link = anchor?.getAttribute('href');
+  // const damUrl = link ? new URL(link) : block.textContent.trim();
+  // const pictureEle = await createPicTagWithOpenApi(damUrl);
 
   const heroBody = document.createElement('div');
 
-  const captionText = caption ? promoteChild(caption) : null;
-  const titleText = title ? promoteChild(title) : null;
-  const introText = text ? promoteChild(text) : null;
-  const isAltTextRequired = imgAlt ? promoteChild(imgAlt) : null;
-  const hideImageOnMobile = hideOnMobile ? promoteChild(hideOnMobile) : null;
+  const titleText = heroTitle ? promoteChild(heroTitle) : null;
+
+  const checkImgAltText = checkImgAlt ? promoteChild(checkImgAlt) : null;
+
+  const mainImage = heroImage ? promoteChild(heroImage) : null;
+
+  const captionText = imgCaption ? promoteChild(imgCaption) : null;
+
+  const logoImage = logoImg ? promoteChild(logoImg) : null;
+
+  const checklogoAltText = checklogoAlt ? promoteChild(checklogoAlt) : null;
+
+  const introTextValue = introText ? promoteChild(introText) : null;
+
+  console.log(checkImgAltText, checklogoAltText);
 
   // Add CSS classes only if the elements exist
   captionText?.classList.add('caption');
   titleText?.classList.add('title');
-  introText?.classList.add('intro-text');
-  heroBody.classList.add('body');
+  introTextValue?.classList.add('introText');
+  heroBody?.classList.add('heroContainer');
+  mainImage?.classList.add('image');
+  logoImage?.classList.add('logo');
 
   // Append only the elements that exist
-  const elementsToAppend = [titleText, pictureEle, captionText, introText].filter(
-    (el) => el !== null,
-  ); // Remove `null` values
+  const elementsToAppend = [
+    titleText,
+    captionText,
+    introText,
+    mainImage,
+    logoImage,
+  ].filter((el) => el !== null); // Remove `null` values
 
   heroBody.append(...elementsToAppend); // Append only existing elements
   block.textContent = '';
   block.append(heroBody);
-  // optimizeImages(block);
-  console.log(heading, logo, logoAlt, isAltTextRequired, hideImageOnMobile);
+  optimizeImages(block);
 }
