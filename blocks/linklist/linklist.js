37305e816d0c8d8f7d8489b9475d36bbb69a9388
Kumar <ankit.hd.kumar@accenture.com>
2025-04-15 18:42:26 +0530

    link list-40

--- blocks/linklist/linklist.js
+++ blocks/linklist/linklist.js
@@ -1,35 +1,12 @@
-// import promoteChild from '../../scripts/utils/dom.js';
+import promoteChild from '../../scripts/utils/dom.js';
 
 export default async function decorate(block) {
   const [headingTitle] = [...block.children];
-  console.log(block, 'linklist decorate function', headingTitle);
-  // generating damURL from anchor tag
-  // const heroBody = document.createElement('div');
-  // const heroContentMax = document.createElement('div');
-  // const heroContent = document.createElement('div');
-  // const titleText = heroTitle ? promoteChild(heroTitle) : null;
-  // const checkImgAltText = checkImgAlt ? promoteChild(checkImgAlt) : null;
-  // const mainImage = heroImage ? promoteChild(heroImage) : null;
-  // const logoImage = logoImg ? promoteChild(logoImg) : null;
-  // const captionText = imgCaption ? promoteChild(imgCaption) : null;
-  // const introTextValue = introText ? promoteChild(introText) : null;
-  // const checklogoAltText = checklogoAlt ? promoteChild(checklogoAlt) : null;
-  // console.log(checkImgAltText, checklogoAltText);
-  // Add CSS classes only if the elements exist
-  // captionText?.classList.add('caption');
-  // titleText?.classList.add('title');
-  // introTextValue?.classList.add('intro-text');
-  // heroBody?.classList.add('hero-body');
-  // mainImage?.classList.add('image');
-  // logoImage?.classList.add('logo');
-  // heroContent?.classList.add('hero-content');
-  // heroContentMax.classList.add('hero-max-content');
-  // const isValidElement = (el) =>
-  //   el && (el.textContent.trim() !== '' || el.querySelector('img')?.getAttribute('src'));
-  // const elementAppendHeroBody = [titleText, mainImage].filter(isValidElement);
-  // const elementAppendHeroContent = [captionText, logoImage, introTextValue].filter(
-  //   isValidElement,
-  // );
+
+  const mainTitle = headingTitle ? promoteChild(headingTitle) : null;
+
+  console.log(block, 'linklist decorate function', mainTitle);
+
   // heroContentMax.append(...elementAppendHeroContent);
   // heroContent.append(heroContentMax);
   // heroBody.append(...elementAppendHeroBody);
