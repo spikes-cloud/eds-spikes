fe53b64b0d6b548b0d9cd8ac95b2fef9bf3ea205
Kumar <ankit.hd.kumar@accenture.com>
2025-04-16 11:50:11 +0530

    link list-41

--- blocks/linklist/linklist.js
+++ blocks/linklist/linklist.js
@@ -5,13 +5,29 @@ export default async function decorate(block) {
 
   const mainTitle = headingTitle ? promoteChild(headingTitle) : null;
 
-  console.log(block, 'linklist decorate function', mainTitle);
+  // generating damURL from anchor tag
+  const linklistBody = document.createElement('div');
+  const linklistItemOne = document.createElement('div');
+  const linklistItemTwo = document.createElement('div');
+  const linklistUl = document.createElement('ul');
+  const linklistli = document.createElement('li');
+  const linklistAnchor = document.createElement('a');
+  const linklistP = document.createElement('p');
 
-  // heroContentMax.append(...elementAppendHeroContent);
-  // heroContent.append(heroContentMax);
-  // heroBody.append(...elementAppendHeroBody);
-  // heroBody.append(heroContent);
-  // block.textContent = '';
-  // block.append(heroBody);
-  // console.log(heroBody, 'hey there ankit');
+  // Add CSS classes only if the elements exist
+  linklistBody?.classList.add('linklist-body');
+  linklistItemOne?.classList.add('linklist-item-one');
+  linklistItemTwo?.classList.add('linklist-item-two');
+  linklistUl?.classList.add('linklist-ul');
+  linklistli?.classList.add('linklist-li');
+
+  console.log(block, 'linklist decorate function', mainTitle);
+  linklistItemOne.append(mainTitle);
+  linklistli.append(linklistAnchor, linklistP);
+  linklistUl.append(linklistli);
+  linklistItemTwo.append(linklistUl);
+  linklistBody.append(linklistItemOne, linklistItemTwo);
+
+  block.textContent = '';
+  block.append(linklistBody);
 }
